e3e2f48ae60507cb2f40889710e9eb0c44c2dbe0
Frank Doyle <Frank.Doyle@zestbenefits.com>
2025-01-29 17:53:05 +0000

    Targeting .net8

--- tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7dd0d33b7d56a2eb1845c045ec7b2228b65353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b193d6be4f945f32d246caba61c3fec376b30b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
