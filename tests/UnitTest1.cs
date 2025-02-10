caa846331ec40b2bfffa34fd617ab39d4802b65a
Frank Doyle <frankdoylezw@users.noreply.github.com>
2025-02-10 15:48:43 +0000

    Update UnitTest1.cs Breaking a Test

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -22,7 +22,7 @@ public class PlaywrightTests : PageTest
     [Test]
     public async Task HomepageLoadsCorrectly()
     {
-        await Page.GotoAsync("https://frankdoylezw.github.io/CSharp_Learning/");
+        await Page.GotoAsync("https://frankdoylezw.github.io/CSharp_Learning01/");
         var title = await Page.TitleAsync();
         Assert.That(title, Does.Contain("Structured Learning Plan for Web Application Development"));
     }
