a465f9698c9887ccb279aef04732586232e2c476
Frank Doyle <Frank.Doyle@zestbenefits.com>
2025-02-10 16:49:09 +0000

    Changes

--- tests/obj/Release/net8.0/tests.AssemblyInfo.cs
+++ tests/obj/Release/net8.0/tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b193d6be4f945f32d246caba61c3fec376b30b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bb59890883a85f7735a4c59f3fba0630bead94")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
-  use: {
-    trace: 'on-first-retry',
-  },
+    reporter: [['html', { outputFolder: 'playwright-report' }]],
+    use: {
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+    },
 });
