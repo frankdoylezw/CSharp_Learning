6c4dc2a06964283ff0e971c7ac0202e23b86b9b0
Frank Doyle <Frank.Doyle@zestbenefits.com>
2025-02-10 19:44:34 +0000

    Local files

--- tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bb59890883a85f7735a4c59f3fba0630bead94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6a86e7af5ebe6154db476aac2c294476cb6e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
